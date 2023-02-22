5879c3605b498565a4320edb71c08fcd16a50ed6
student <student@B206-12>
2023-02-22 08:43:18 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WeatherApp
+{
+    partial class WeatherForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeatherDataGrid = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WeatherDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WeatherDataGrid
+            // 
+            this.WeatherDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.WeatherDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.WeatherDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.WeatherDataGrid.Name = "WeatherDataGrid";
+            this.WeatherDataGrid.ReadOnly = true;
+            this.WeatherDataGrid.Size = new System.Drawing.Size(776, 196);
+            this.WeatherDataGrid.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(392, 225);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(162, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите город";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin"});
+            this.comboBox1.Location = new System.Drawing.Point(392, 274);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сброс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сортировка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // WeatherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.WeatherDataGrid);
+            this.Name = "WeatherForm";
+            this.Text = "Погода Хакасии";
+            ((System.ComponentModel.ISupportInitialize)(this.WeatherDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView WeatherDataGrid;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherForm : Form
+    {
+        private WeatherDatabase _weather;
+
+        public WeatherForm()
+        {
+            InitializeComponent();
+
+            _weather = new WeatherDatabase();
+            _weather.Initialize();
+
+            WeatherDataGrid.DataSource = _weather.Weathers.ToList();
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text)) { WeatherDataGrid.DataSource = _weather.Weathers; return; }
+            WeatherDataGrid.DataSource = _weather.Weathers
+                .Where(t => t.CityName.ToLower().Contains(textBox1.Text.ToLower()))
+                .ToList();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            WeatherDataGrid.DataSource = _weather.Weathers;
+            textBox1.Text =string.Empty;
+            comboBox1.Text = string.Empty;
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, System.EventArgs e)
+        {
+            if(comboBox1.SelectedItem !=null)
+            {
+                var vb = comboBox1.SelectedItem as string;
+                WeatherDataGrid.DataSource = _weather.Weathers
+                    .Where(n=>n.MeasureUnit.ToString()==vb)
+                    .ToList();
+                return;
+            }
+            WeatherDataGrid.DataSource = _weather.Weathers;
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            WeatherDataGrid.DataSource = _weather.Weathers
+                .OrderBy(k => k.Temperature)
+                .ToList();
+        }
+    }
+}
